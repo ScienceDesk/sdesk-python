841797dca9b9e565d62a9f8cf0fe170cce995d0e
Daniel Debonzi <debonzi@gmail.com>
2020-05-02 17:53:13 -0300

    Test development conditional deployment using same version.

--- sdesk/__init__.py
+++ sdesk/__init__.py
@@ -1,1 +1,18 @@
+import os
+import sys
+import requests
+
 __version__ = "0.2.2+rc.1"
+
+
+def check_published(uri, extension=".tar.gz"):
+    url = os.path.join(uri, "sdesk", "sdesk-" + __version__ + extension)
+    sys.stdout.write(str(requests.head(url).ok))
+
+
+def published():
+    if len(sys.argv) <= 1:
+        sys.exit(1)
+
+    uri, extension = sys.argv[1:3] if len(sys.argv) > 2 else [sys.argv[1], ".tar.gz"]
+    check_published(uri, extension)
